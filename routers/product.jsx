e1f0e275d65c1cfe4230b92faf6e86b898c7808f
luizfiorentino <lgfiorentino@gmail.com>
2022-09-15 21:04:10 +0200

    GET all products and GET product with category endpoints added

--- routers/product.jsx
+++ routers/product.jsx
@@ -1,10 +1,33 @@
 const { Router } = require("express");
 const router = new Router();
 const Product = require("../models").product;
+const Category = require("../models").category;
 
 router.get("/", async (req, res, next) => {
-  const allProducts = await Product.findAll();
-  res.send(allProducts);
+  try {
+    const allProducts = await Product.findAll({
+      include: { model: Category },
+    });
+    res.send(allProducts);
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const productId = parseInt(req.params.id);
+    const specificProduct = await Product.findByPk(productId, {
+      include: { model: Category },
+    });
+    if (!specificProduct) {
+      res.status(404).send({ message: "product with provided id not found" });
+    } else {
+      res.send(specificProduct);
+    }
+  } catch (e) {
+    next(e);
+  }
 });
 
 module.exports = router;
